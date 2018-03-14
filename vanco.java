5c46506b3642c328361077dec020036331b6dcc4
Adam O'Brien <17226864@studentmail.ul.ie>
2018-03-14 13:23:27 +0000

    Initial commit
    Added layout of files
    First round of tasks assigned
    Added .gitignore

---
+++ vanco.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+
+public class vanco{
+
+    // Contains info about each admin
+    ArrayList<ArrayList> adminDetails = new ArrayList<ArrayList>();
+
+    // Contains info about each league
+    ArrayList<ArrayList> leagueDetails = new ArrayList<ArrayList>();
+
+    // Contains info about currently selected league
+    // must be updated if changing leagues
+    ArrayList<ArrayList> leagueTeams = new ArrayList<ArrayList>();
+
+    // Contains fixture info about currently selected league
+    // must be updated if changing leagues
+    ArrayList<ArrayList> fixtureDetails = new ArrayList<ArrayList>();
+
+    // Contains result info about currently selected league
+    // must be updated if changing leagues
+    ArrayList<ArrayList> resultDetails = new ArrayList<ArrayList>();
+
+    /* --- ToDo --- 
+
+    Write data from arrays to files
+
+    LeagueData
+    FixtureData
+    ResultsData
+
+     --- End ToDo --- */
+
+    public vanco(){
+        init();
+        // Call your methods from here
+
+        // E.g.
+        helloWorld();
+    }
+
+    public void helloWorld(){
+        System.out.println("Hello World");
+    }
+
+
+    public void init(){
+        // This will run at the beginning when file is ran
+        adminDetails.add(new ArrayList<Integer>());     // AdminID
+        adminDetails.add(new ArrayList<String>());      // AdminUsername
+        adminDetails.add(new ArrayList<String>());      // AdminPassword
+
+        leagueDetails.add(new ArrayList<Integer>());    // AdminID
+        leagueDetails.add(new ArrayList<Integer>());    // LeagueID
+        leagueDetails.add(new ArrayList<String>());     // LeagueTitle
+
+        leagueTeams.add(new ArrayList<Integer>());      // TeamID
+        leagueTeams.add(new ArrayList<String>());       // TeamName
+
+        fixtureDetails.add(new ArrayList<Integer>());   // GameID
+        fixtureDetails.add(new ArrayList<Integer>());   // Team1ID
+        fixtureDetails.add(new ArrayList<Integer>());   // Team2ID
+
+        resultDetails.add(new ArrayList<Integer>());    // GameID
+        resultDetails.add(new ArrayList<String>());     // ResultType (w = win, d = draw)
+        resultDetails.add(new ArrayList<Integer>());    // WinnerID (-1 if draw)
+
+        // Insert dummy info into adminDetails and leagueDetails
+        for( int i = 0; i < 10; i++ ){
+            adminDetails.get(0).add(i);
+            adminDetails.get(1).add("adminusername" + i);
+            adminDetails.get(2).add("adminpassword" + i);
+
+            leagueDetails.get(0).add(i);
+            leagueDetails.get(1).add(i);
+            leagueDetails.get(2).add("leaguetitle" + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        new vanco();
+    }
+}
