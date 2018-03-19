b8448f0bfb857086da38314f95b5c75d88f35a1f
Adam O'Brien <17226864@studentmail.ul.ie>
2018-03-19 19:54:48 +0000

    Tedis fix error

--- leagueProject.java
+++ leagueProject.java
@@ -66,45 +66,43 @@ public class leagueProject{
 		return false;
     }
     // Tedis Stumbrs
-    public static boolean attemptLogin(Arraylist Arr){
-    	public String Password, Username, cPassword, cUsername;
+    public static boolean attemptLogin(){
+    	String Password, Username, cPassword, cUsername;
     	Username = JOptionPane.showInputDialog(null, "Please enter your Username");
 		Password = JOptionPane.showInputDialog(null, "Please enter your Password");
-    	for(int i = 0; i < Arr.get(1).size(); i++){
-    		cUsername = Arr.get(1).get(i);
-    		cPassword = Arr.get(2).get(i);
+    	for(int i = 0; i < adminDetails.get(1).size(); i++){
+    		cUsername = adminDetails.get(1).get(i).toString();
+    		cPassword = adminDetails.get(2).get(i).toString();
     		if(cUsername.equals(Username) && cPassword.equals(Password)){
     			adminID = i;
     			return true;
     		}
-    	}
+		}
+		return false;
     }
 
-	public static createNewLeague(){
+	public static void createNewLeague(){
 		// Ask user for league title
 		// Ask user to add teams to the league
 		// Show league details to user
 		// Ask if user accepts the details
 		// Save data to files
-		return false;
 	}
 
 	// Tedis Stumbrs
 	public static void editLeague(int leagueID){
 		// Display options about which part of the league needs to be changed
-		if(attemptLogin() == true){
-		int dir;
-		dir = JOptionPane.showInputDialog(null, "To display all leagues and alter a league title, Enter \"0\" " + "\n" + "To Add or Remove teams from a 
-			selected league, Enter \"1\"" + "\n" + "To check results of each game within a league or to alter results, Enter \"2\"")
-		leagueDirectory(dir);
-
+		if(attemptLogin()){
+			int dir;
+			dir = Integer.parseInt(JOptionPane.showInputDialog(null, "To display all leagues and alter a league title, Enter " + 0 + " \nTo Add or Remove teams from a selected league, Enter \"1\"\nTo check results of each game within a league or to alter results, Enter \"2\""));
+			leagueDirectory(dir);
 		}
 		// Title
 		// Ask user to input new title
 
 		// Teams
-		// Allow user to remove teams from arraylist
-		// Allow user to add teams to arraylist
+		// Allow user to remove teams from ArrayList
+		// Allow user to add teams to ArrayList
 
 		// Results
 		// Loop through each game
@@ -119,66 +117,67 @@ public class leagueProject{
 	// If login is successful will direct to a list of leagues owned by the admin
 	// Separate total leagues from leagues created by admin
 		if(dir == 0){
-		public Arraylist<String> usersLeagues = new ArrayList<String>();
-		public String selectedLeague, newTitle;
-		for(int i = 0; i < leagueDetails.size(); i++){
-			if(leagueDetails.get(0).get(i) == adminID){
-				usersLeagues.add(leagueDetails.get(2).get(i));
+			ArrayList<String> usersLeagues = new ArrayList<String>();
+			String selectedLeague, newTitle;
+			for(int i = 0; i < leagueDetails.size(); i++){
+				if(leagueDetails.get(0).get(i) == adminID){
+					usersLeagues.add(leagueDetails.get(2).get(i).toString());
+				}
 			}
-		}
-		// Display list of leagues that the user created
-		JOptionPane.showMessageDialog(null, "Here is a list of all leagues owned by " + Username + ":" + "\n" + usersLeagues);
-		// Prompt user to select a league from the list
-		selectedLeague0 = JOptionPane.showInputDialog(null, "Which league would you like to change?");
-		// Ask user to enter new title for selected league
-		newTitle = JOptionPane.showInputDialog(null, "Enter new league title");
-		// Simply switching the titles of the leagues for both the total leagues and users leagues
-		for(int i = 0; i < leagueDetails.size(); i++){
-			if(leagueDetails.get(2).get(i) == selectedLeague0){
-				leagueDetails.get(2).get(i) = newTitle;
+			// Display list of leagues that the user created
+			JOptionPane.showMessageDialog(null, "Here is a list of all leagues owned by " + Username + ":" + "\n" + usersLeagues);
+			// Prompt user to select a league from the list
+			selectedLeague0 = JOptionPane.showInputDialog(null, "Which league would you like to change?");
+			// Ask user to enter new title for selected league
+			newTitle = JOptionPane.showInputDialog(null, "Enter new league title");
+			// Simply switching the titles of the leagues for both the total leagues and users leagues
+			for(int j = 0; j < leagueDetails.size(); j++){
+				if(leagueDetails.get(2).get(j) == selectedLeague0){
+					newTitle = leagueDetails.get(2).get(j);
+				}
 			}
-		}
-		for(int i = 0; i < usersLeagues.size(); i++){
-			if(usersLeagues.get(i) == selectedLeague0){
-				usersLeagues.get(i) = newTitle;
+			for(int k = 0; k < usersLeagues.size(); k++){
+				if(usersLeagues.get(k) == selectedLeague0){
+					newTitle = usersLeagues.get(k);
+				}
 			}
+			// Display list of leagues post edit
+			JOptionPane.showMessageDialog(null, "Here is an updated list of all leagues owned by " + Username + ":"+ "\n" + usersLeagues);
 		}
-		// Display list of leagues post edit
-		JOptionPane.showMessageDialog(null, "Here is an updated list of all leagues owned by " + Username + ":"+ "\n" + usersLeagues);
-	}
-	// User wishes to access teams in a league
-	if(dir == 1){
-		ArayList<String> teams = new ArrayList<String>;
-		ArrayList<String> removedTeams = new ArrayList<String>;
-		String selectedLeague1, newTeam, removedTeam;
-		int id, dir1, newID;
-		// Display list of leagues belonging to user
-		JOptionPane.showMessageDialog(null, "Here is a list of all leagues owned by " + Username + ":" + "\n" + usersLeagues);
-		selectedLeague1 = JOptionPane.showInputDialog(null, "Which league would you like to access?");
-		// Loop through selected league matching League ID to leagueTeam ID 
-		for(int i = 0; i < leagueDetails.size(); i++){
-			if(leagueDetails.get(2).get(i) == selectedleague1){
-				id = i;
+		// User wishes to access teams in a league
+		if(dir == 1){
+			ArayList<String> teams = new ArrayList<String>();
+			ArrayList<String> removedTeams = new ArrayList<String>();
+			String selectedLeague1, newTeam, removedTeam;
+			int id, dir1, newID;
+			// Display list of leagues belonging to user
+			JOptionPane.showMessageDialog(null, "Here is a list of all leagues owned by " + Username + ":" + "\n" + usersLeagues);
+			selectedLeague1 = JOptionPane.showInputDialog(null, "Which league would you like to access?");
+			// Loop through selected league matching League ID to leagueTeam ID 
+			for(int i = 0; i < leagueDetails.size(); i++){
+				if(leagueDetails.get(2).get(i) == selectedleague1){
+					id = i;
+				}
 			}
-		}
-		// Adding teams with matching ID's to seperate arraylist for this specific leagues teams
-		for(int i = 0; i < leagueTeams.size(); i ++){
-			if(leagueTeams.get(0).get(i) == id){
-				teams.add(leagueTeams.get(0).get(i));		
+			// Adding teams with matching ID's to seperate ArrayList for this specific leagues teams
+			for(int i = 0; i < leagueTeams.size(); i ++){
+				if(leagueTeams.get(0).get(i) == id){
+					teams.add(leagueTeams.get(0).get(i));		
+				}
 			}
-		}
-		// Display teams in selected league
-		JOptionPane.showMessageDialog(null, "Here is a list of all the teams in " + selectedLeague1 + ":" + "\n" + teams);
-		// Create another directory for adding or removing teams
-		dir1 = JOptionPane.showInputDialog(null, "To add a new team to the league, Enter : \"1\" " + "\n" "To remove a team from the league, Enter : \"2\"")
-		// Add new team
-		if(dir1 == 1){
-			newTeam = JOptionPane.showInputDialog(null, "Enter new team name");
-		// Add new team to new arraylist for league specific teams
-			teams.add(newTeam);
-		// Add new team and new team ID to main list of teams 
-			leagueTeams.get(1).add(newTeam);
-			leagueTeams.get(0).add(id);
+			// Display teams in selected league
+			JOptionPane.showMessageDialog(null, "Here is a list of all the teams in " + selectedLeague1 + ":" + "\n" + teams);
+			// Create another directory for adding or removing teams
+			dir1 = JOptionPane.showInputDialog(null, "To add a new team to the league, Enter : \"1\" \nTo remove a team from the league, Enter : \"2\"");
+			// Add new team
+			if(dir1 == 1){
+				newTeam = JOptionPane.showInputDialog(null, "Enter new team name");
+			// Add new team to new ArrayList for league specific teams
+				teams.add(newTeam);
+			// Add new team and new team ID to main list of teams 
+				leagueTeams.get(1).add(newTeam);
+				leagueTeams.get(0).add(id);
+			}
 		}
 		// Remove a team
 /*		if(dir1 == 2){
@@ -193,18 +192,14 @@ public class leagueProject{
 		// Will complete tomorrow
 	}
 
-}
-
-
-
-	public static generateResults(int leagueID){
+	public static void generateResults(int leagueID){
 		// Check that results have been entered for selected league
 		// Calculate points for each team
 		// Display list with teams (points descending)
 		// Save generated results to file
 	}
 
-	public static generateFixtures(int leagueID){
+	public static void generateFixtures(int leagueID){
 		// Look through annettes code for fixture generation
 	}
 
